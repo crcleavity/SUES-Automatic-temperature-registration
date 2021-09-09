6ec7598480aa3b6fd674d1b82614a68a6890ac9e
BillyBai <82216907+BillyBai@users.noreply.github.com>
2021-09-09 12:33:38 +0800

    Add files via upload
    version2.0 update

---
+++ version2.0/config.py
@@ -0,0 +1,14 @@
+user_dict = {
+    "user": {
+        "account": "",  # 你的校园网账号
+        "password": "",  # 你的校园网密码
+        "mail": ""  # 你的邮箱
+    }
+    # 可以添加多个用户，参照字典格式
+}
+
+smtp_dict = {
+    "from_name": "",  # 发件人姓名
+    "from_address": "",  # 发件人邮箱
+    "from_password": ""  # email smtp address
+}
